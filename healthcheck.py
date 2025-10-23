64c494ac3a729a259375ea4895a1ecddb47e3b19
KJZOFFERS <krisrealestate112@gmail.com>
2025-10-22 20:58:47 -0400

    Delete healthcheck.py

--- healthcheck.py
+++
@@ -1,9 +0,0 @@
-from aiohttp import web
-
-async def health(request):
-    return web.json_response({"status": "ok"})
-
-def start_health_server():
-    app = web.Application()
-    app.router.add_get("/health", health)
-    web.run_app(app, port=8080)

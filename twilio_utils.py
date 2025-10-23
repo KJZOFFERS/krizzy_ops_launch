214625797da505b5cfcd0dad2388cf401f4473cc
KJZOFFERS <krisrealestate112@gmail.com>
2025-10-22 21:02:48 -0400

    Refactor send_sms to simplify SMS sending
    Refactor send_sms function to remove SAFE_MODE check and streamline message sending.

--- twilio_utils.py
+++ twilio_utils.py
@@ -1,14 +1,10 @@
-import os
 from twilio.rest import Client
-
-SAFE_MODE = True
-client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
-SERVICE_SID = os.getenv("TWILIO_MESSAGING_SERVICE_SID")
+import os
 
 def send_sms(to, body):
-    if SAFE_MODE:
-        return
-    try:
-        client.messages.create(messaging_service_sid=SERVICE_SID, body=body, to=to)
-    except Exception:
-        pass
+    client = Client(os.environ["TWILIO_ACCOUNT_SID"], os.environ["TWILIO_AUTH_TOKEN"])
+    client.messages.create(
+        messaging_service_sid=os.environ["TWILIO_MESSAGING_SERVICE_SID"],
+        to=to,
+        body=body
+    )

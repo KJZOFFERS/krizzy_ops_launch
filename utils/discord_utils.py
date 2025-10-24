c093fdf7ae283dfbfd5c8aa3f8053b1961c90bee
KJZOFFERS <krisrealestate112@gmail.com>
2025-10-24 18:34:06 -0400

    Add function to send messages to Discord channels

--- utils/discord_utils.py
+++ utils/discord_utils.py
@@ -1,1 +1,10 @@
+import requests, os
 
+def send_message(channel_env, message):
+    url = os.getenv(channel_env)
+    if not url:
+        return
+    try:
+        requests.post(url, json={"content": message})
+    except Exception:
+        pass

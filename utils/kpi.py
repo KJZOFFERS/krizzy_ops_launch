a0ec9d3cc7a02b966b76e5a24c4c3a8abe00614d
KJZOFFERS <krisrealestate112@gmail.com>
2025-10-24 18:33:21 -0400

    Add push function to log events to Airtable

--- utils/kpi.py
+++ utils/kpi.py
@@ -1,1 +1,13 @@
+from pyairtable import Table
+import os, time, json
+
+def push(event, data):
+    api = os.getenv("AIRTABLE_API_KEY")
+    base = os.getenv("AIRTABLE_BASE_ID")
+    table = os.getenv("TABLE_KPI_LOG", "KPI_Log")
+    Table(api, base, table).create({
+        "event": event,
+        "data": json.dumps(data),
+        "timestamp": int(time.time())
+    })
 

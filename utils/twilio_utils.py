79086151e25f9b01b70f74325e30a0243c4b9b1e
KJZOFFERS <krisrealestate112@gmail.com>
2025-10-24 18:34:29 -0400

    Implement SMS sending functionality with Twilio
    Added function to send SMS using Twilio API.

--- utils/twilio_utils.py
+++ utils/twilio_utils.py
@@ -1,1 +1,10 @@
+from twilio.rest import Client
+import os
 
+def send_sms(to, body):
+    client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
+    client.messages.create(
+        to=to,
+        from_=os.getenv("TWILIO_MESSAGING_SERVICE_SID"),
+        body=body
+    )

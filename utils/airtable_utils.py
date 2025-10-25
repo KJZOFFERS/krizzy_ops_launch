4ec1c98d082213d644cf5944019b34ce8e181696
KJZOFFERS <krisrealestate112@gmail.com>
2025-10-25 18:29:11 -0400

    Implement write_record function for Airtable
    Add function to write records to Airtable

--- utils/airtable_utils.py
+++ utils/airtable_utils.py
@@ -1,1 +1,9 @@
+from pyairtable import Table
+import os
 
+def write_record(table_name, data):
+    base_id = os.getenv("AIRTABLE_BASE_ID")
+    api_key = os.getenv("AIRTABLE_API_KEY")
+    table = Table(api_key, base_id, table_name)
+    table.create(data)
+    return True

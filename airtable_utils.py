0f0ee4adb16bacf1e03267d6717d104335c40cd3
KJZOFFERS <krisrealestate112@gmail.com>
2025-10-25 17:48:44 -0400

    Delete airtable_utils.py

--- airtable_utils.py
+++
@@ -1,6 +0,0 @@
-from pyairtable import Table
-import os
-
-def write_record(table_name, data):
-    t = Table(os.getenv("AIRTABLE_API_KEY"), os.getenv("AIRTABLE_BASE_ID"), table_name)
-    t.create(data)

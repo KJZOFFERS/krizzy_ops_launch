dfd2843405d8a444bd09b47bf636003b1749e0e7
KJZOFFERS <krisrealestate112@gmail.com>
2025-10-26 00:36:44 -0400

    Update twilio_utils.py

--- utils/twilio_utils.py
+++ utils/twilio_utils.py
@@ -1,10 +1,7 @@
 from twilio.rest import Client
 import os
 
-def send_sms(to, body):
+def send_sms(to_number, body):
     client = Client(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
-    client.messages.create(
-        to=to,
-        from_=os.getenv("TWILIO_MESSAGING_SERVICE_SID"),
-        body=body
-    )
+    messaging_sid = os.getenv("TWILIO_MESSAGING_SERVICE_SID")
+    client.messages.create(to=to_number, messaging_service_sid=messaging_sid, body=body)

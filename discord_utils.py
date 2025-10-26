2f03411fca1022229548959933d579f9fa59c605
KJZOFFERS <krisrealestate112@gmail.com>
2025-10-26 00:38:25 -0400

    Delete discord_utils.py

--- discord_utils.py
+++
@@ -1,7 +0,0 @@
-import requests, os
-
-def send_ops(msg):
-    requests.post(os.environ["DISCORD_WEBHOOK_OPS"], json={"content": msg})
-
-def send_error(msg):
-    requests.post(os.environ["DISCORD_WEBHOOK_ERRORS"], json={"content": f"⚠️ {msg}"})

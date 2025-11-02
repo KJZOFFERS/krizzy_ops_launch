b99e7413cfbb6ed3108249beb3837e9cd4ca3558
KJZOFFERS <krisrealestate112@gmail.com>
2025-11-02 08:01:29 -0500

    Update kpi.py

--- utils/kpi.py
+++ utils/kpi.py
@@ -1,6 +1,8 @@
 from utils.airtable_utils import write_record
 
-def kpi_push(event, data):
+def kpi_push(event_type: str, payload: dict):
+    return write_record("KPI_Log", {"event_type": event_type, **payload})
+
     fields = {"Event": event}
     fields.update(data)
     write_record("KPI_Log", fields)

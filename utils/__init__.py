7ca135f8b58d6672cc062195541805cdf1ec13c4
KJZOFFERS <krisrealestate112@gmail.com>
2025-11-02 17:12:38 -0500

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,12 @@
-from .airtable_utils import airtable_write, write_record
-from .discord_utils import post_ops, post_error, send_message
+from .discord_utils import post_ops, post_error, send_message  # re-export for legacy imports
+from .airtable_utils import create_record, list_records, update_record, kpi_log, now_iso
+__all__ = [
+    "post_ops",
+    "post_error",
+    "send_message",
+    "create_record",
+    "list_records",
+    "update_record",
+    "kpi_log",
+    "now_iso",
+]

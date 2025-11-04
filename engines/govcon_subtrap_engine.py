f9d6ca33bce4db1324388b37d96f9f9cf0848886
KJZOFFERS <krisrealestate112@gmail.com>
2025-11-03 23:50:13 -0500

    Update govcon_subtrap_engine.py

--- engines/govcon_subtrap_engine.py
+++ engines/govcon_subtrap_engine.py
@@ -1,13 +1,41 @@
-import time
+from utils import list_records, kpi_log
 from utils.discord_utils import post_ops
+import os
 
-last_govcon_run = None
+OPP_TABLE = os.getenv("AT_TABLE_GOVCON", "GovCon_Opportunities")
+NAICS_WHITELIST = os.getenv("NAICS_WHITELIST", "").replace(" ", "").split(",")
+MAX_DAYS = int(os.getenv("GOVCON_POSTED_DAYS", 14))
+
 
 def loop_govcon():
-    global last_govcon_run
+    opps = list_records(OPP_TABLE)
 
-    # --- YOUR GOVCON PROCESSING LOGIC LIVES HERE ---
-    # Do not delete. Replace with your actual GovCon workflow when ready.
+    for opp in opps:
+        f = opp.get("fields", {})
+        key = f.get("key")
+        name = f.get("Opportunity Name")
+        agency = f.get("Agency")
+        value = f.get("Total Value")
+        naics = str(f.get("NAICS Code", "")).replace(" ", "").split(",")
+        days = f.get("Days Until Deadline")
 
-    last_govcon_run = int(time.time())
-    post_ops(f"[GOVCON] cycle complete at {last_govcon_run}")
+        if not set(naics).intersection(NAICS_WHITELIST):
+            continue
+        if days and days > MAX_DAYS:
+            continue
+
+        post_ops(f"""
+**GovCon Qualified Opportunity**
+Name: `{name}`
+Agency: {agency}
+Value: ${value:,.0f}
+NAICS: {', '.join(naics)}
+Days Left: {days}
+""")
+
+        kpi_log(
+            engine="GovCon_SubTrap",
+            action="Qualified Opportunity",
+            record_id=key,
+            details={"days_left": days}
+        )

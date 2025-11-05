a5ea3fb04e7d57bdbb7f4a7844dfc5b82db2adc2
KJZOFFERS <krisrealestate112@gmail.com>
2025-11-05 02:53:45 -0500

    Update __init__.py

--- engines/__init__.py
+++ engines/__init__.py
@@ -1,5 +1,7 @@
-from . import rei_dispo_engine
-from . import govcon_subtrap_engine
+# makes `from engines import rei_dispo_engine, govcon_subtrap_engine` valid
+import importlib
+
+rei_dispo_engine = importlib.import_module(".rei_dispo_engine", __name__)
+govcon_subtrap_engine = importlib.import_module(".govcon_subtrap_engine", __name__)
 
 __all__ = ["rei_dispo_engine", "govcon_subtrap_engine"]
-

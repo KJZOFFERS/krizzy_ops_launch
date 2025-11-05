ed532f2b7eb7776ddc4cb72c4d22eb691e813e33
KJZOFFERS <krisrealestate112@gmail.com>
2025-11-05 10:03:45 -0500

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,26 @@
-__all__ = ["list_records", "create_record", "update_record", "upsert_record"]
+# FILE: utils/__init__.py
+# Import-safe lazy exports
+__all__ = [
+    "list_records", "create_record", "update_record", "upsert_record",
+    "safe_airtable_write", "safe_upsert",
+    "fetch_table", "resolve_table",
+]
 
 def __getattr__(name: str):
     if name in __all__:
-        from .airtable_utils import list_records, create_record, update_record, upsert_record
+        from .airtable_utils import (
+            list_records, create_record, update_record, upsert_record,
+            safe_airtable_write, safe_upsert,
+            fetch_table, resolve_table,
+        )
         return {
             "list_records": list_records,
             "create_record": create_record,
             "update_record": update_record,
             "upsert_record": upsert_record,
+            "safe_airtable_write": safe_airtable_write,
+            "safe_upsert": safe_upsert,
+            "fetch_table": fetch_table,
+            "resolve_table": resolve_table,
         }[name]
     raise AttributeError(name)

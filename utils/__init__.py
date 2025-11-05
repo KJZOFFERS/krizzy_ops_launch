b77ea2fb3a46670969162ab68311c428b88ab5e0
KJZOFFERS <krisrealestate112@gmail.com>
2025-11-05 10:53:01 -0500

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,26 +1,20 @@
-# FILE: utils/__init__.py
-# Import-safe lazy exports
+from .airtable_utils import (
+    list_records,
+    create_record,
+    update_record,
+    upsert_record,
+    safe_airtable_write,
+)
+from .discord_utils import post_ops, post_error, send_message  # convenience re-exports
+
 __all__ = [
-    "list_records", "create_record", "update_record", "upsert_record",
-    "safe_airtable_write", "safe_upsert",
-    "fetch_table", "resolve_table",
+    "list_records",
+    "create_record",
+    "update_record",
+    "upsert_record",
+    "safe_airtable_write",
+    "post_ops",
+    "post_error",
+    "send_message",
 ]
 
-def __getattr__(name: str):
-    if name in __all__:
-        from .airtable_utils import (
-            list_records, create_record, update_record, upsert_record,
-            safe_airtable_write, safe_upsert,
-            fetch_table, resolve_table,
-        )
-        return {
-            "list_records": list_records,
-            "create_record": create_record,
-            "update_record": update_record,
-            "upsert_record": upsert_record,
-            "safe_airtable_write": safe_airtable_write,
-            "safe_upsert": safe_upsert,
-            "fetch_table": fetch_table,
-            "resolve_table": resolve_table,
-        }[name]
-    raise AttributeError(name)

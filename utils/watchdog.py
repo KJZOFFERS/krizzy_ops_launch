1738d96bb3b9b91837beca1a39f18fc4475b86cf
KJZOFFERS <krisrealestate112@gmail.com>
2025-11-05 10:04:55 -0500

    Update watchdog.py

--- utils/watchdog.py
+++ utils/watchdog.py
@@ -1,6 +1,11 @@
-import asyncio, logging
+# FILE: watchdog.py
+import asyncio
+from utils.discord_utils import post_ops
 
 async def heartbeat():
     while True:
-        logging.info("KRIZZY OPS heartbeat active")
+        try:
+            post_ops("KRIZZY OPS heartbeat active")
+        except Exception:
+            pass
         await asyncio.sleep(60)

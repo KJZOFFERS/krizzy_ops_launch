28361e983a83651b543aca31a63492b1ed6595eb
KJZOFFERS <krisrealestate112@gmail.com>
2025-11-05 10:06:51 -0500

    Update twilio_utils.py

--- utils/twilio_utils.py
+++ utils/twilio_utils.py
@@ -1,8 +1,17 @@
+# FILE: twilio_utils.py
 import os
-from twilio.rest import Client
 
-def send_sms(to, body):
+def send_sms(to_phone: str, body: str) -> dict:
+    """
+    Lazy import Twilio to avoid boot-time dependency if unused.
+    Requires env: TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_FROM
+    """
+    from twilio.rest import Client  # type: ignore
     sid = os.getenv("TWILIO_ACCOUNT_SID")
-    token = os.getenv("TWILIO_AUTH_TOKEN")
-    svc = os.getenv("TWILIO_MESSAGING_SERVICE_SID")
-    Client(sid, token).messages.create(messaging_service_sid=svc, to=to, body=body)
+    tok = os.getenv("TWILIO_AUTH_TOKEN")
+    from_num = os.getenv("TWILIO_FROM")
+    if not (sid and tok and from_num):
+        raise RuntimeError("Twilio env vars missing")
+    cli = Client(sid, tok)
+    msg = cli.messages.create(to=to_phone, from_=from_num, body=body)
+    return {"sid": msg.sid, "status": msg.status}

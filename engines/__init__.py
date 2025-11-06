42f8921a77af0c847bf5c0aa57895b54341964f8
KJZOFFERS <krisrealestate112@gmail.com>
2025-11-06 10:03:23 -0500

    Simplify engines package imports

--- engines/__init__.py
+++ engines/__init__.py
@@ -1,3 +1,3 @@
-from . import rei_dispo_engine as rei_dispo_engine
-from . import govcon_subtrap_engine as govcon_subtrap_engine
-__all__ = ["rei_dispo_engine", "govcon_subtrap_engine"]
+from . import govcon_subtrap_engine, rei_dispo_engine
+
+__all__ = ["govcon_subtrap_engine", "rei_dispo_engine"]

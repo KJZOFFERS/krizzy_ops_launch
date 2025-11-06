381c99f64c37aa203e0420826327a89399441b0e
KJZOFFERS <krisrealestate112@gmail.com>
2025-11-05 21:43:56 -0500

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,4 @@
 from .airtable_utils import list_records, upsert_record
 from .discord_utils import post_ops, post_error
 
-__all__ = [
-    "list_records",
-    "upsert_record",
-    "post_ops",
-    "post_error",
-]
+__all__ = ["list_records", "upsert_record", "post_ops", "post_error"]

dd09f5168dd049a42f0300e5866da7b877dcb081
KJZOFFERS <krisrealestate112@gmail.com>
2025-11-26 19:15:15 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,12 @@
+# src/__init__.py
+
 from .airtable_client import AirtableClient
+# existing imports:
+from . import ops_health_service, govcon_subtrap_engine, rei_dispo_engine
+
+__all__ = [
+    "AirtableClient",
+    "ops_health_service",
+    "govcon_subtrap_engine",
+    "rei_dispo_engine",
+]

f46185d0c4f7f9481c6240cacb608621cd1ea3b7
KJZOFFERS <krisrealestate112@gmail.com>
2025-11-26 19:33:27 -0500

    Refactor Airtable imports in __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,12 @@
 # src/__init__.py
 
-from .airtable_client import AirtableClient
-# existing imports:
+from .common.airtable_client import AirtableClient, AirtableSchemaError, AirtableError
 from . import ops_health_service, govcon_subtrap_engine, rei_dispo_engine
 
 __all__ = [
     "AirtableClient",
+    "AirtableSchemaError",
+    "AirtableError",
     "ops_health_service",
     "govcon_subtrap_engine",
     "rei_dispo_engine",

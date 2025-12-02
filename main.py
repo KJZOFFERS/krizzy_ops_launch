00b5e69c27554a6a611e2549efd6cc513da401ed
KJZOFFERS <krisrealestate112@gmail.com>
2025-12-01 20:55:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-import os
-import uvicorn
-from src.app import app
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+from src.app import app  # FastAPI app exposed for Uvicorn

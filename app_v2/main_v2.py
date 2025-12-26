36535e9b723cc19bb2a8a91f55305689b9398a50
KJZOFFERS <krisrealestate112@gmail.com>
2025-12-26 14:39:53 -0500

    fix: ensure uvicorn start command and dependency

--- app_v2/main_v2.py
+++ app_v2/main_v2.py
@@ -76,5 +76,8 @@ def trigger_underwriting():
 
 
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+
+    port = int(os.getenv("PORT", "8080"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
